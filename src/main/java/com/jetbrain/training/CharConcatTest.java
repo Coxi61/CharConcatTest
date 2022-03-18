64dcac674724f02d99941c9eaa122be3e1ef5afb
Coxi61 <101890128+Coxi61@users.noreply.github.com>
2022-03-18 20:47:27 +0100

    Initial commit

---
+++ src/main/java/com/jetbrain/training/CharConcatTest.java
@@ -0,0 +1,28 @@
+/*
+Characters  Adding symbols
+ Medium
+22313 users solved this problem. Latest completion was
+about 2 hours ago
+.
+What does the following code print?
+
+char c = '1' + '3';
+System.out.println(c);
+ */
+
+package com.jetbrain.training;
+
+public class CharConcatTest {
+    public static void main(String[] args) {
+        char c = '1' + '4';
+        System.out.println(c);
+
+        //'1'+'3' =
+        // 0x31 = 48+1=49
+        // 0x33 = 48+3=51
+        // 100 = 6*16 + 4 = 0x64 -> unicode character -> 'd'
+
+        boolean b = true;
+
+    }
+}
